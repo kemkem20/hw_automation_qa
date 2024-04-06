f26f57dfca64dc6ca0e94a18d4083725f5028fce
kemkem20 <mrkem792@gmail.com>
2024-04-06 14:58:50 +0300

    Hw4 fix

--- src/main/java/org/example/homework4/LeapYear.java
+++ src/main/java/org/example/homework4/LeapYear.java
@@ -8,8 +8,11 @@ public class LeapYear {
         } else System.out.println(year + " isn't a Leap Year or out of range [1;9999]");
     }
 
-    private static boolean isLeapYear(int Year) {
-        if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0)) {
+    private static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             return true;
         } else return false;
     }
