24faa3f94b32b59094628bc54957e790bcb35f70
kemkem20 <164179118+kemkem20@users.noreply.github.com>
2024-04-09 14:19:55 +0300

    Hw6 done (#6)

---
+++ src/main/java/org/example/homework6/ArrayReverse.java
@@ -0,0 +1,23 @@
+package org.example.homework6;
+
+import java.util.Arrays;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int[] array = {1, 33, 55, 77, 88, 3};
+        reverseArray(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    private static void reverseArray(int[] array) {
+        int size = array.length;
+        for (int i = 0; i < size/2; i++) {
+            int temp = array[i];
+            array[i] = array[size - 1 - i];
+            array[size - 1 - i] = temp;
+
+
+        }
+    }
+}
